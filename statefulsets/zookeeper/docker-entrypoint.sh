37834596b01acbae335fd9d2a659166364171c43
guox <guox@goodrain.com>
2018-06-14 14:53:12 +0800

    [fix] management memory of JVM

--- statefulsets/zookeeper/docker-entrypoint.sh
+++ statefulsets/zookeeper/docker-entrypoint.sh
@@ -147,11 +147,57 @@ function create_log_props () {
     echo "Wrote log4j configuration to $LOGGER_PROPS_FILE"
 }
 
+# set default_java_mem_opts
+case ${MEMORY_SIZE:-small} in
+    "micro")
+       export default_java_mem_opts="-Xms90m -Xmx90m -Xss512k  -XX:MaxDirectMemorySize=12M"
+       echo "Optimizing java process for 128M Memory...." >&2
+       ;;
+    "small")
+       export default_java_mem_opts="-Xms180m -Xmx180m -Xss512k -XX:MaxDirectMemorySize=24M "
+       echo "Optimizing java process for 256M Memory...." >&2
+       ;;
+    "medium")
+       export default_java_mem_opts="-Xms360m -Xmx360m -Xss512k -XX:MaxDirectMemorySize=48M"
+       echo "Optimizing java process for 512M Memory...." >&2
+       ;;
+    "large")
+       export default_java_mem_opts="-Xms720m -Xmx720m -Xss512k -XX:MaxDirectMemorySize=96M "
+       echo "Optimizing java process for 1G Memory...." >&2
+       ;;
+    "2xlarge")
+       export default_java_mem_opts="-Xms1420m -Xmx1420m -Xss512k -XX:MaxDirectMemorySize=192M"
+       echo "Optimizing java process for 2G Memory...." >&2
+       ;;
+    "4xlarge")
+       export default_java_mem_opts="-Xms2840m -Xmx2840m -Xss512k -XX:MaxDirectMemorySize=384M "
+       echo "Optimizing java process for 4G Memory...." >&2
+       ;;
+    "8xlarge")
+       export default_java_mem_opts="-Xms5680m -Xmx5680m -Xss512k -XX:MaxDirectMemorySize=768M"
+       echo "Optimizing java process for 8G Memory...." >&2
+       ;;
+    16xlarge|32xlarge|64xlarge)
+       export default_java_mem_opts="-Xms8G -Xmx8G -Xss512k -XX:MaxDirectMemorySize=1536M"
+       echo "Optimizing java process for biger Memory...." >&2
+       ;;
+    *)
+       export default_java_mem_opts="-Xms128m -Xmx128m -Xss512k -XX:MaxDirectMemorySize=24M"
+       echo "Optimizing java process for 256M Memory...." >&2
+       ;;
+esac
+
+if [[ "${JAVA_OPTS}" == *-Xmx* ]]; then
+  export JAVA_TOOL_OPTIONS=${JAVA_TOOL_OPTIONS:-"-Dfile.encoding=UTF-8"}
+else
+  default_java_opts="${default_java_mem_opts} -Dfile.encoding=UTF-8"
+  export JAVA_OPTS="${default_java_opts} $JAVA_OPTS"
+  
 function create_java_env() {
     rm -f $JAVA_ENV_FILE
     echo "Creating JVM configuration file"
     echo "ZOO_LOG_DIR=$ZK_LOG_DIR" >> $JAVA_ENV_FILE
-    echo "JVMFLAGS=\"-Xmx$ZK_HEAP_SIZE -Xms$ZK_HEAP_SIZE\"" >> $JAVA_ENV_FILE
+    echo "JVMFLAGS=${JAVA_OPTS}" >> $JAVA_ENV_FILE
     echo "Wrote JVM configuration to $JAVA_ENV_FILE"
 }
 
